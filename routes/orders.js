735d79603b4f9a8fcc72881a03c78f2d826046f0
Andrew Hoang <ahoang18@bu.edu>
2016-10-22 23:41:15 -0400

    Particle event working

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+var request = require('request');
 var parse_sms = require('./parse_sms.js');
+
+var Particle = require('particle-api-js');
+var particle = new Particle();
+var secrets = require('./secrets.json');
+var token = null;
+
+particle.login({username: secrets.email, password: secrets.pw}).then(
+  function(data){
+    console.log('API call completed on promise resolve: ', data.body.access_token);
+    token = data.body.access_token;
+  },
+  function(err) {
+    console.log('API call completed on promise fail: ', err);
+  }
+);
 
 /* Heartbeat */
 router.get('/', function(req, res, next) {
-  res.status(200);
+  res.status(200).end();
 });
 
 router.post('/', function(req, res, next) {
@@ -13,7 +28,19 @@ router.post('/', function(req, res, next) {
   var json = parse_sms.getOrder(req.body.Body);
   console.log("Order json: " + JSON.stringify(json, null, 2));
   // console.log(parse_sms.getOrder("apple ice"));
-  res.status(200);
+  // console.log("particle token: " + particle.token);
+  particle.publishEvent({ name: 'blendr',
+    data: json,
+    private: true,
+    auth: token }).then(
+    function(data) {
+      if (data.body.ok) { console.log("Event published succesfully") }
+    },
+    function(err) {
+      console.log("Failed to publish event: " + err)
+    }
+  );
+  res.sendStatus(200).end();
 });
 
 module.exports = router;
