6d722a9857780fa60f92bc68ecd39ca062cf0e2d
Andrew Hoang <ahoang18@bu.edu>
2016-10-22 21:07:13 -0400

    Add function header

---
+++ routes/parse_sms.js
@@ -0,0 +1,17 @@
+
+//Return json object with this structure:
+// {
+//    fruit1: boolean,
+//    fruit2: boolean,
+//    fruit3: boolean,
+//    ice: boolean,
+//    juice: boolean,
+//    name: string,
+//    phone: number
+// }
+
+function getOrder(text){
+
+}
+
+module.exports.getOrder = getOrder:
